78300c34d24afba15137c5c5ce576c6523636311
Alfonso Kim <alfonsokim84@gmail.com>
2014-03-02 12:49:34 -0600

    avances ejercicio 1

--- cache/cache.c
+++ cache/cache.c
@@ -18,40 +18,34 @@ double sumaInvertida(double *arreglo, long N) {
 
 // ************************************************************************
 // ************************************************************************
-double sumaEnIncrementos(double *arreglo, long tamArreglo, double factor) {
+void sumaEnIncrementos(double *arreglo, long tamArreglo, double factor) {
 
-    long N = 100; // invocar la rutina desde 100 hasta el tamanio del arreglo
-    long operaciones = 0;
-    long salto = N * factor; // aqui guardo el incremento a c de cada iteracion
+    long N = 100;           // invocar la rutina desde 100 hasta el tamanio del arreglo
+    long salto = N * factor;// aqui guardo el incremento a c de cada iteracion
+
+    clock_t inicio, fin;    // "para cada valor de N (calculado con el factor)
+                            // mida el tiempo que toma hacer las R llamadas a
+                            // la funcion suma"
     
-    for(; N <= tamArreglo; N += salto) { // "multiplicar el valor por una constante" 
-        long suma = sumaInvertida(arreglo, N);
-        operaciones += N; // sumaInvertida hace N operaciones
+    for(; N <= tamArreglo; N += salto) {        // "multiplicar el valor por una constante" 
+        inicio = clock();
+        long suma = sumaInvertida(arreglo, N);  // r llamadas a la funcion suma
+        fin = clock();
+        double tiempoMS = ((double)(fin - inicio) / CLOCKS_PER_SEC * 1000);
+        printf("N=%i, t=%f ms\n", N, tiempoMS);
     }
-    printf("%d operaciones para factor=%f\n", operaciones, factor);
-}
-
-// ************************************************************************
-// ************************************************************************
-double prueba(double *arreglo, long tamArreglo, double factor) {
-
-    clock_t startTime, endTime;
-    startTime = clock();
-    sumaEnIncrementos(arreglo, tamArreglo, factor);
-    endTime = clock();
-    double timeInMS = (double)(endTime - startTime) / CLOCKS_PER_SEC * 1000;
-    // printf("Tiempo en sumar N elementos del arreglo: %f ms\n", (double)(endTime - startTime) / CLOCKS_PER_SEC * 1000);    
-    // printf("Suma: %f\n", sum);
-
 
 }
 
 // ************************************************************************
 // ************************************************************************
-int main(){
-    srand ( time(NULL) );
+int main() {
 
-    long tamArreglo = 1000000;
+    srand( time(NULL) );
+
+    // printf("tam double %d\n", sizeof(double)); // 8 Bytes :)
+
+    long tamArreglo = 1000000; //FIXME: No funciona con 10000000 :(
     double arreglo[tamArreglo];
 
     long c; // llenar el arreglo
@@ -59,11 +53,7 @@ int main(){
         arreglo[c] = ( (double)rand() / (double)RAND_MAX );
     }
 
-    long N = 1000;
-    long R = 1000;
-
-    printf("tam double %f", sizeof(double));
-    prueba(arreglo, tamArreglo, 1.2);
+    sumaEnIncrementos(arreglo, tamArreglo, 1.5);
 
     return 0;
 }
