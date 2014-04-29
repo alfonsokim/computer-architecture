a4d1999a994d0af22359e860fa481d725bd9bea9
Alfonso Kim <alfonsokim84@gmail.com>
2014-04-29 09:15:22 -0500

    ejercicio 1 de OpenMP

---
+++ openmp/HelloWorlds.c
@@ -0,0 +1,18 @@
+// #include <omp.h>
+#include <stdio.h>
+
+int main() {
+
+    #pragma omp parallel
+    {
+        int i;
+    	printf("Hello World\n");
+    	
+        #pragma omp for
+        for(i = 0; i < 6; i++) {
+    		printf("Iter:%d\n",i);
+        }
+    }
+
+	printf("GoodBye World\n");
+}
