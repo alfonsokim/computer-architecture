a9c1fbee8a214b5d88edb56a4699d076df3ad461
Alfonso Kim <alfonsokim84@gmail.com>
2014-05-01 18:52:21 -0500

    avances en lo de los primos

--- openmp/primos_multi.c
+++ openmp/primos_multi.c
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-//    CÃ³digo adaptado de Intel (2005)
+//    Codigo adaptado de Intel (2005)
 ///////////////////////////////////////////////////////////////////////////////
 //
 // Argumentos:  Rango inicial, rango final
@@ -10,12 +10,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-//#include <windows.h>
+#include <omp.h>
 #include <time.h>
 #include <math.h>
 static       int gProgress    = 0,
                  gPrimesFound = 0;   // Acumulador de nÃºmero de primos encontrados
-long             globalPrimes[1000000];  // Para almacenar los primos encontrados
+long             globalPrimes[1000000000];  // Para almacenar los primos encontrados
 int              CLstart, CLend;
 
 
@@ -33,14 +33,15 @@ void ShowProgress( int val, int range )
     
     percentDone = (int)((float)gProgress/(float)range *200.0f + 0.5f);
     
-	if( percentDone % 10 == 0 )
+	if( percentDone % 10 == 0 ) {
         printf("\b\b\b\b%3d%%", percentDone);
+    }
 	
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 //
-//  Busca factorres.  Devuelve "0" si los encuentra
+//  Busca factores.  Devuelve "0" si los encuentra
 //
 ///////////////////////////////////////////////////////////////////////////////
 
@@ -48,7 +49,7 @@ int TestForPrime(int val)
 {
     int limit, factor = 3;
     
-    limit = (long)(sqrtf((float)val)+0.5f);
+    limit = (long)( sqrtf( ( float )val ) + 0.5f );
     while( (factor <= limit) && (val % factor))
         factor ++;
     
@@ -58,14 +59,24 @@ int TestForPrime(int val)
 void FindPrimes(int start, int end)
 {
     // start siempre es non
-    int i,range = end - start + 1;
+    int i, range = end - start + 1;
     
-    for( i = start; i <= end; i += 2 )
+    //#pragma omp parallel private(i)
     {
-        if( TestForPrime(i) )
-            globalPrimes[gPrimesFound++] = i;
-        
-        ShowProgress(i, range);
+
+        #pragma omp for private(i)
+        for( i = start; i <= end; i += 2 )
+        {
+            if( TestForPrime(i) ) {
+                #pragma omp critical  
+                {
+                    globalPrimes[gPrimesFound++] = i;
+                }
+            }
+            
+            //ShowProgress(i, range);
+        }
+
     }
 }
 
@@ -73,7 +84,7 @@ void FindPrimes(int start, int end)
 int main(int argc, char **argv)
 {
     int     start, end,i;
-    clock_t before, after;
+    //clock_t before, after;
     
     if( argc == 3 )
     {
@@ -91,7 +102,7 @@ int main(int argc, char **argv)
         exit(-1);
     }
     if (CLstart < 1 && CLend < 2) {
-		printf("El rango de bÃºsqueda debe estar formado por enteros positivos\n");
+		printf("El rango de busqueda debe estar formado por enteros positivos\n");
 		exit(-1);
     }
     start = CLstart;
@@ -103,15 +114,26 @@ int main(int argc, char **argv)
         globalPrimes[gPrimesFound++] = 2;
     
     if((start % 2) == 0 )
-        start = start + 1;  // Asegurar que iniciamos con un nÃºmero impar
+        start = start + 1;  // Asegurar que iniciamos con un numero impar
     
 
-    before = clock();
-    FindPrimes(start, end);
-    after = clock();
-    
-    printf("\n\nSe encontraron %8d primos entre  %6d y %6d en %7.2f secs\n",
-           gPrimesFound, CLstart, CLend, (float)(after - before)/ CLOCKS_PER_SEC);
+    long num_threads, end_num;
+    for (num_threads = 2; num_threads < 10; num_threads += 2){
+        for(end_num = 10000000; end_num < 10000000*10; end_num *= 2){
+
+            start = 1;
+            //end = end_num;
+            omp_set_num_threads(num_threads);
+            double t_start = omp_get_wtime();
+            FindPrimes(start, end_num);
+            double t_end = omp_get_wtime();
+
+            printf("Primos=%d,Max=%d,Tiempo=%.2f,Hilos=%d\n",
+            gPrimesFound, end_num, (t_end - t_start), num_threads);
+
+            gPrimesFound = 0;
+        }    
+    }
     
 }
 
